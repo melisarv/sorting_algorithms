3ad7f7ceddad23a49991677c6bf7297d6ea3d6c1
Melisa Rojas <melisarv@gmail.com>
2021-06-15 19:35:28 -0500

    function that sorts an array of integers - bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers using the Bubble sort algorithm
+ * @array: pointer array of integers
+ * @size: size of array
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, endsize;
+
+	endsize = size - 1;
+	while (endsize != 0)
+	{
+
+		for (i = 0; i < endsize; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		endsize--;
+	}
+}
